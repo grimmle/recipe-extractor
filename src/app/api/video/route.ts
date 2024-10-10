f091b989f8231df9d281fbb6c3517d9cadde452a
Lennard Grimm <hi@grimmle.com>
2024-10-10 15:54:32 +0200

    change route back to GET

--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -24,14 +24,14 @@ function handleError(error: any) {
   }
 }
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   if (!INSTAGRAM_CONFIGS.enableServerAPI) {
     const notImplementedResponse = makeErrorResponse("Not Implemented");
     return NextResponse.json(notImplementedResponse, { status: 501 });
   }
 
-  const postData = await request.json();
-  const postUrl = postData.postUrl;
+  const postUrl = new URL(request.url).searchParams.get("postUrl");
+
   if (!postUrl) {
     const badRequestResponse = makeErrorResponse("Post URL is required");
     return NextResponse.json(badRequestResponse, { status: 400 });
